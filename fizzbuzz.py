e5fbedb6871b7f00da5216b263b1c5b80a87c119
Stephen Shaw <stephen.shaw@softwire.com>
2019-08-08 17:01:37 +0100

    Implement Part 2.3.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,24 +1,24 @@
 def main():
-    for i in range(1, 106):
-        printing_number = True
-        
-        if i % 11 == 0:
-            print('Bong')
-            continue
+    for i in range(1, 144):
+        outputs = []
 
-        if i % 3 == 0:
-            printing_number = False
-            print('Fizz', end='')
-        if i % 5 == 0:
-            printing_number = False
-            print('Buzz', end='')
-        if i % 7 == 0:
-            printing_number = False
-            print('Bang', end='')
-        
-        if printing_number:
-            print(i, end='')
-        
-        print()
+        if i % 11 == 0:
+            outputs.append('Bong')
+
+        if i % 3 == 0 and 'Bong' not in outputs:
+            outputs.append('Fizz')
+        if i % 5 == 0 and 'Bong' not in outputs:
+            outputs.append('Buzz')
+        if i % 7 == 0 and 'Bong' not in outputs:
+            outputs.append('Bang')
+            
+        if i % 13 == 0:
+            index = ([i for i, j in enumerate(outputs) if j[0] == 'B'] or [len(outputs)])[0]
+            outputs.insert(index, 'Fezz')
+
+        if not outputs:
+            outputs.append(str(i))
+
+        print(str(i) + ': ' + ''.join(outputs))
 
 if __name__ == "__main__": main()
